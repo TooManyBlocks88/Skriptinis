7a1a713e42f5a8d4d3b59360ee2bc0679e0bf49e
Bernardas06 <bernardaspetraska@gmail.com>
2026-09-08 13:05:42 +0300

    Update LD1_BernardasPetraska.m

--- LD1/LD1_BernardasPetraska.m
+++ LD1/LD1_BernardasPetraska.m
@@ -2,3 +2,14 @@
 % EEf-25/2
 % 2026-09-08
 
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-]   |   F_2 [-x-]')
+
+help sin
+doc plot
+lookfor title
